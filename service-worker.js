5e55ba70eb434e29a86268f8fda04fa112870600
Your Name <freelspringisoul@gmail.com>
2018-12-21 17:51:39 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Weather_App/build/precache-manifest.78d408a00963ff2c13a898a9e94eeddc.js"
+  "/Weather_App/precache-manifest.b54b19762720c26e2bc4c7c85e748d07.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Weather_App/build/index.html", {
+workbox.routing.registerNavigationRoute("/Weather_App/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
